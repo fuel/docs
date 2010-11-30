534f1c04238cab1e3c0e980d5934e714aaf818e3
Dan Horrigan <dan@dhorrigan.com>
2010-11-30 11:27:49 -0500

    Adding some "Basic" docs.  Some style changes.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -8,8 +8,12 @@
 function show_nav(path)
 {
 	document.write(
+		'<h3>Basic</h3>'+
 		'<ul>'+
 		'<li><a href="'+path+'index.html">Home</a></li>'+
+		'<li><a href="'+path+'requirements.html">Requirements</a></li>'+
+		'<li><a href="'+path+'license.html">License</a></li>'+
+		'<li><a href="'+path+'credits.html">Credits</a></li>'+
 		'</ul>'
 	);
 }
