f818b24eee91ec2d9b11c23c7e3f1c87728f9010
John Snyder <juven14@gmail.com>
2010-11-30 13:32:56 -0500

    Added collapsible nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,15 +5,41 @@
  * It extracts the navigation to a single file for easier updating.
  */
 
-function show_nav(path)
+//define document navigation
+var nav = {
+		"Basic": {
+			"Home":"index.html",
+			"Requirements":"requirements.html",
+			"License":"license.html",
+			"Credits":"credits.html"
+		},
+		//example group
+		"Group1": {
+			"Item 1":"group1/item1.html",
+			"Item 2":"group1/item2.html"
+		}
+};
+
+//insert the navigation
+function show_nav(page)
 {
-	document.write(
-		'<h3>Basic</h3>'+
-		'<ul>'+
-		'<li><a href="'+path+'index.html">Home</a></li>'+
-		'<li><a href="'+path+'requirements.html">Requirements</a></li>'+
-		'<li><a href="'+path+'license.html">License</a></li>'+
-		'<li><a href="'+path+'credits.html">Credits</a></li>'+
-		'</ul>'
-	);
+	$.each(nav, function(section,links) {
+		var h3 = $('<h3></h3>');
+		h3.addClass('collapsible').html(section);
+		h3.attr('id', section.toLowerCase().replace(' ', ''));
+		h3.bind('click', function() {
+			$(this).next('div').slideToggle();
+		});
+		$('nav').append(h3);
+		var div = $('<div></div>');
+		if (page != h3.attr('id')) {
+			div.hide();
+		}
+		var ul = div.append('<ul></ul>');
+		$.each(links, function(title, href) {
+			ul.find('ul').append('<li><a href="'+href+'">'+title+'</a></li>');
+		});
+		$('nav').append(div);
+		$('nav').find('#'+page).next('div').slideDown();
+	});
 }
