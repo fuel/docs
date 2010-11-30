494e7b9288932437d4cad7937e4771a435aa6d38
Dan Horrigan <dan@dhorrigan.com>
2010-11-30 14:36:13 -0500

    The nav now adds an active class to the current page in the navigation.  Also cleaned up some formatting and added a path option for when pages are in sub-folders.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -8,16 +8,18 @@
 //define document navigation
 var nav = {
 		"Basic": {
-			"Home":"index.html",
-			"Requirements":"requirements.html",
-			"License":"license.html",
-			"Credits":"credits.html"
+			"Home":			"index.html",
+			"Requirements":	"requirements.html",
+			"License":		"license.html",
+			"Credits":		"credits.html"
 		}
 };
 
 //insert the navigation
-function show_nav(page)
+function show_nav(page, path)
 {
+	active_path = window.location.pathname;
+	path = path == null ? '' : path;
 	$.each(nav, function(section,links) {
 		var h3 = $('<h3></h3>');
 		h3.addClass('collapsible').html(section);
@@ -25,15 +27,23 @@ function show_nav(page)
 		h3.bind('click', function() {
 			$(this).next('div').slideToggle();
 		});
+
 		$('nav').append(h3);
 		var div = $('<div></div>');
 		if ('nav_'+page != h3.attr('id')) {
 			div.hide();
 		}
+
 		var ul = div.append('<ul></ul>');
 		$.each(links, function(title, href) {
-			ul.find('ul').append('<li><a href="'+href+'">'+title+'</a></li>');
+			active = '';
+			if (active_path.indexOf(href, active_path.length - href.length) != -1)
+			{
+				active = ' class="active"';
+			}
+			ul.find('ul').append('<li><a href="'+path+href+'"'+active+'>'+title+'</a></li>');
 		});
+
 		$('nav').append(div);
 		$('nav').find('#nav_'+page).next('div').slideDown();
 	});
