f611a35fe021e2ba7efb6df91770ba822aaf04b2
Frank de Jonge <info@frenky.net>
2011-04-02 01:56:33 -0700

    typo

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -52,7 +52,7 @@ var nav = {
 			"Crypt":		"classes/crypt.html",
 			// "Database":		"todo.html",
 			 "Date":			"classes/date.html",
-			"DB":		"classes/db.htmll",
+			"DB":		"classes/db.html",
 			// "DButil":		"todo.html",
 			"Debug":		"classes/debug.html",
 			// "Email":		"todo.html",
