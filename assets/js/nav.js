46701452060864636239b21b29620596b5eebe80
FrenkyNet <info@frenky.net>
2011-04-13 02:19:58 +0200

    DB docs part one, the basics

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -50,8 +50,15 @@ var nav = {
 			"Cookie":		"classes/cookie.html",
 			"Crypt":		"classes/crypt.html",
 			// "Database":		"todo.html",
-			 "Date":			"classes/date.html",
-			"DB":		"classes/db.html",
+			"Date":			"classes/date.html",
+			"Database": {
+				"Introduction":     "classes/database/introduction.html",
+				//"Configuration":     "classes/database/configuration.html",
+				"Usage":     "classes/database/usage.html",
+				"DB class":     "classes/database/db.html"
+				//"DBUtil class":     "todo.html",
+			},
+			//"DB":		"classes/db.html",
 			// "DButil":		"todo.html",
 			"Debug":		"classes/debug.html",
 			// "Email":		"todo.html",
