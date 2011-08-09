923d188e31d86770ef4384032e4bf04738a60459
Mark Kasaboski <mark.tech1@gmail.com>
2011-08-09 13:26:16 -0400

    Forgot to add ORM
    Added ORM

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -151,7 +151,7 @@ var nav = {
 			},
 			"Writing drivers":	"packages/auth/drivers.html"
 		},
-		"Orm Package": {
+		"ORM Package": {
 			"Introduction":		"packages/orm/intro.html",
 			"Creating Models":	"packages/orm/creating_models.html",
 			"CRUD":				"packages/orm/crud.html",
