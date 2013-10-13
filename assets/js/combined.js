a62804ca823c3da1b9cf0ce06ecf83b5cf769dee
WanWizard <wanwizard@wanwizard.eu>
2013-10-13 21:28:57 +0200

    fixed incorrect CSE selection

--- assets/js/combined.js
+++ assets/js/combined.js
@@ -2,10 +2,10 @@
 var DocVersion = "1.8-dev";
 
 // set the google CSE key based on the docversion
-if (DocVersion.indexOf('-dev')) {
-	var GoogleCSE = "006268519150918941486:avkdxx7rmmc";
-} else {
+if (DocVersion.indexOf('-dev') == -1) {
 	var GoogleCSE = "006268519150918941486:vg1sweimb7m";
+} else {
+	var GoogleCSE = "006268519150918941486:avkdxx7rmmc";
 }
 
 /*! jQuery v1.9.1 | (c) 2005, 2012 jQuery Foundation, Inc. | jquery.org/license
