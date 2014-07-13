27715e5adc20e0026db770f6ad768d3261531137
WanWizard <wanwizard@wanwizard.eu>
2014-07-13 15:01:36 +0200

    bumped the version to 1.7.2

--- assets/js/combined.js
+++ assets/js/combined.js
@@ -1,5 +1,5 @@
 /* set the page title, including the version of these docs */
-var DocVersion = "1.7.1";
+var DocVersion = "1.7.2";
 
 // set the google CSE key based on the docversion
 if (DocVersion.indexOf('-dev') == -1) {
